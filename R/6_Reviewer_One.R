73bbfb1b2d8325b49a88797f3c62b05ce2a8526c
FloodHydrology <cnjones7@ua.edu>
2023-09-20 12:21:28 -0500

    9/20/2023 morning updates

--- R/6_Reviewer_One.R
+++ R/6_Reviewer_One.R
@@ -17,8 +17,9 @@ library(dataRetrieval)
 library(parallel)
 library(patchwork)
 
-
-# Are error distributions normally distributed ---------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Comment #3 Are error distributions normally distributed ----------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Load data
 gages<-read_csv('data/gages.csv')
 sd<-read_csv("temp/sd.csv")
@@ -62,4 +63,119 @@ s_test<-lapply(seq(1,nrow(gages)), shapiro_fun) %>% bind_rows()
 
 #how many gages above 0.05
 s_test %>% filter(p_value>=0.01) %>% nrow()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Comment #5 The nubmer of paired- stage-discharge measurement as a driver of error-----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Clear R environment
+remove(list=ls())
+
+#load uncertainty results
+df<-read_csv("data//uncertainty_results.csv")
+
+#load sd curve
+sd <- read_csv("temp/sd.csv")
+
+#load ts data
+ts <- read_csv("temp/ts.csv")
+
+#estimate the number of stage-discharge curve measurements
+sd <- sd %>% 
+  filter(type=='raw') %>% 
+  group_by(site_no) %>% 
+  summarise(n_meas = n()) 
+
+#Combine with df
+df <- df %>% left_join(., sd)
+
+#correlation test
+cor.test(df$Q_uncertainty, df$n_meas, method = 'spearman')
+
+#plot
+sd_plot <- df %>% 
+  filter(n_meas<3000) %>% 
+  ggplot(aes(x = n_meas, y = Q_uncertainty)) + 
+  #Add line data
+  geom_point(col="steelblue2", pch=19, alpha=0.7, cex=3) +
+  #Add predefined black/white theme
+  theme_bw() +
+  #Change font size of axes
+  theme(
+    axis.title = element_text(size = 14), 
+    axis.text  = element_text(size = 10)
+  ) + 
+  #Add labels
+  xlab("Number of Stage-Discharge Measurements") + 
+  ylab("Q Relative Error (%)") 
+
+#define length of flow record
+ts <- ts %>% 
+  mutate(date = lubridate::date(datetime)) %>% 
+  group_by(site_no, date) %>% 
+  summarise(Q_cfs = mean(Q_cfs, na.rm=T))
+
+#Determine length of flow record
+ts <- ts %>% 
+  group_by(site_no) %>% 
+  summarise(age_yr = n()/365)
+
+#Combine with master df 
+df <- df %>% left_join(., ts)
+
+#correlation test
+cor.test(df$Q_uncertainty, df$age_yr, method = 'spearman')
+
+#plot
+age_plot <- df %>% 
+  filter(n_meas<3000) %>% 
+  ggplot(aes(x = age_yr, y = Q_uncertainty)) + 
+  #Add line data
+  geom_point(col="steelblue2", pch=19, alpha=0.7, cex=3) +
+  #Add predefined black/white theme
+  theme_bw() +
+  #Change font size of axes
+  theme(
+    axis.title = element_text(size = 14), 
+    axis.text  = element_text(size = 10)
+  ) + 
+  #Add labels
+  xlab("Gauge Record Length (Years)") + 
+  ylab("Q Relative Error (%)") 
+
+age_plot + sd_plot + plot_annotation(tag_levels = "A")
+
+ggsave("docs/si_fig.png", 
+        width = 12, 
+        height = 5.15, 
+        units = "in",
+        dpi=300)
   
+  
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Comment #6 Checking ws area in Figure 1 --------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Clear R environment
+remove(list=ls())
+
+#load uncertainty results
+df    <- read_csv("data//uncertainty_results.csv")
+gages <- read_csv("data//gages.csv")
+
+#recreate figure 1c
+df %>% 
+  left_join(., gages) %>% 
+  ggplot(aes(x=drain_area_va, y = Q_uncertainty )) + 
+  geom_point() +#Add predefined black/white theme
+  theme_bw() +
+  #Change font size of axes
+  theme(
+    axis.title = element_text(size = 14), 
+    axis.text  = element_text(size = 10)
+  ) + 
+  #Add labels
+  xlab("watershed area") + 
+  ylab("Q Relative Error (%)")+ 
+  #Scale axes
+  scale_x_log10()
+
+#wabash
